a17635942d6d6ae5fbfe5736aa8e2ee830c97ba2
Attie, Raphael (GSFC-6710)[UNIVERSITIES SPACE RESEARCH ASSOCIATION] <raphael.attie@nasa.gov>
2019-03-05 00:36:55 -0500

    Changed the download structure. Updated mask creation to explicitly exclude blank masks. Added mapping of files to hek_times as csv files

--- JP2_Image_Download.py
+++ JP2_Image_Download.py
@@ -19,42 +19,42 @@ import datetime
 from datetime import timedelta
 import os, glob, shutil
 from mahotas.polygon import fill_polygon
-
+import csv
 
 
 
 
 class Jp2ImageDownload:
 
-    def __init__(self, parent_dir='', full_image_set=False, dt=timedelta(minutes=30),
+    def __init__(self, save_dir, full_image_set=False, dt=timedelta(minutes=30),
                  tstart='2012/05/30 23:59:59', tend='2012/05/31 23:59:59'):
         """
         Set up initial start and ending times and save directory.
 
-        :param parent_dir: Directory to contain the full subdirectory tree of images and masks
+        :param save_dir: Directory to contain the full subdirectory tree of images and masks
         :param full_image_set: boolean if true will run through the entire SDO catalogue
         :param tstart: starting time string of any format read by parse_time
         :param tend: ending time string of any format read by parse_time
         :param dt: limit the time gap between downloaded image and requested hek time. Set to None for no limit.
         """
 
-        if full_image_set:
-            tstart = '2010/05/31 23:59:59'
-            tend = '2018/12/31 23:59:59'
+        # if full_image_set:
+        #     tstart = '2010/05/31 23:59:59'
+        #     tend = '2018/12/31 23:59:59'
 
-        if parent_dir:
-            self.parent_dir = os.path.abspath(parent_dir)
-        else:
-            self.parent_dir = os.path.abspath(os.curdir)
-            print('Using current directory as save directory: {:s}'.format(self.parent_dir))
+        self.save_dir = os.path.abspath(save_dir)
 
-        # Format string for the date-based subdirectories created under self.save_dir
-        self.data_subdir_format = '%Y_%m_%d'
         # Subdirectory for discarding the files from incomplete sets
-        self.reject_subdir = 'incomplet_set'
+        self.reject_dir = os.path.join(self.save_dir, 'incomplete_set')
+        # Create the "rejection" subdirectory to move the images from incomplete groups
+        os.makedirs(self.reject_dir, exist_ok=True)
+        # Directory to save the label masks
+        self.label_save_dir = os.path.join(self.save_dir, 'label_masks')
+        os.makedirs(self.label_save_dir, exist_ok=True)
 
         self.tstart = parse_time(tstart)
         self.tend = parse_time(tend)
+        self.times = []
 
         self.date_list, self.date_string_list  = self.gen_date_list()
         # time gap tolerated between requested time and image time s.t. time_in - dt < actual image time < time_in + dt
@@ -70,7 +70,8 @@ class Jp2ImageDownload:
         self.inst_file_map.append('SDO_HMI_HMI_magnetogram')
         # store the list of files in the save directory
         self.jp2f = []
-
+        self.mask_hek_time_map_csv = os.path.join(self.save_dir, 'mask_hek_time_map.csv')
+        self.hek_time_jp2_map_csv = os.path.join(self.save_dir, 'hek_time_jp2_map.csv')
         # Let's separate the image download from the initialization
         # self.download_images()
 
@@ -83,7 +84,7 @@ class Jp2ImageDownload:
         """
         time_between = self.tend - self.tstart
         ndays = max(1, time_between.days)
-        date_list = [self.tstart + timedelta(days=dd) for dd in range(0, ndays)]
+        date_list = [self.tstart + timedelta(days=dd) for dd in range(0, ndays+1)]
         date_string_list = [tt.strftime('%Y/%m/%d %H:%M:%S') for tt in date_list]
         return date_list, date_string_list
 
@@ -118,65 +119,54 @@ class Jp2ImageDownload:
         Sets up a subdirectory tree to organize the images
         """
 
-        for ii, download_date in enumerate(self.date_list):
-            subdir = download_date.strftime(self.data_subdir_format)
-            save_path = os.path.join(self.parent_dir, subdir)
-            os.makedirs(save_path, exist_ok=True)
-            # First download the images. Feature extraction will be done separately
-            tstart = download_date
-            tend = tstart + timedelta(days=1)
+        # for ii, download_date in enumerate(self.date_list):
+        # First download the images. Feature extraction will be done separately
 
-            # Get a list of existing files (if any)
-            jp2f = sorted(glob.glob(os.path.join(save_path, '*.jp2')))
-            # Parse filenames to get the actual image time
-            jp2_datetimes = [datetime_from_filename(filename) for filename in jp2f]
-            # Get hek event times
-            _, times = get_hek_result(tstart, tend)
+        # Get a list of existing files (if any)
+        jp2f = sorted(glob.glob(os.path.join(self.save_dir, '*.jp2')))
+        # Parse filenames to get the actual image time
+        jp2_datetimes = [datetime_from_filename(filename) for filename in jp2f]
+        # Get hek event times
+        _, self.times = get_hek_result(self.tstart, self.tend)
 
-            for i, time_in in enumerate(times):
-                # Check that we haven't already downloaded some data for that hek time.
-                # Skip them if we did and download only the missing ones.
-                hek_time = parse_time(time_in)
-                measurements = self.list_missing_measurements(hek_time, jp2f, jp2_datetimes)
+        for i, time_in in enumerate(self.times):
+            # Check that we haven't already downloaded some data for that hek time.
+            # Skip them if we did and download only the missing ones.
+            hek_time = parse_time(time_in)
+            measurements = self.list_missing_measurements(hek_time, jp2f, jp2_datetimes)
 
-                if measurements:
-                    print('Checking available data for hek time {:s} at index {:d}'.format(
-                        hek_time.strftime('%Y/%m/%d %H:%M:%S'), i))
-                    image_files, image_times = download_sdo_images(hek_time, measurements, dt=self.dt, save_path=save_path)
-                    for measure_idx, fpath in enumerate(image_files):
-                        if fpath is None:
-                            print('...Skipped measurement {:s} at time {:s} '.format(measurements[measure_idx],
-                                                                                     image_times[measure_idx].strftime(
-                                                                                         '%Y_%m_%d %H:%M:%S')))
-                        else:
-                            print('...downloaded file(s) {:s}'.format(fpath))
-                else:
-                    print('skipping already downloaded data for hek time {:s}'.format(
-                        hek_time.strftime('%Y/%m/%d %H:%M:%S')))
+            if measurements:
+                print('Checking available data for hek time {:s} at index {:d}'.format(
+                    hek_time.strftime('%Y/%m/%d %H:%M:%S'), i))
+                image_files, image_times = download_sdo_images(hek_time, measurements, dt=self.dt, save_path=self.save_dir)
+                for measure_idx, fpath in enumerate(image_files):
+                    if fpath is None:
+                        print('...Skipped measurement {:s} at time {:s} '.format(measurements[measure_idx],
+                                                                                 image_times[measure_idx].strftime(
+                                                                                     '%Y_%m_%d %H:%M:%S')))
+                    else:
+                        print('...downloaded file(s) {:s}'.format(fpath))
+            else:
+                print('skipping already downloaded data for hek time {:s}'.format(
+                    hek_time.strftime('%Y/%m/%d %H:%M:%S')))
 
-            self.data_cleanup(times, save_path)
-            print('Finished download of group {:d}/{:d}'.format(ii+1, len(self.date_list)))
+        self.data_cleanup()
+        print('Finished download')
 
 
-    def data_cleanup(self, times, save_path):
+    def data_cleanup(self):
         """
         Cleanup the downloaded image to have only complete groups in the training set
-
-        :param times: list of all hek times
-        :param save_path: directory where the jpeg2000 files were downloaded
         """
-
+        hek_time_jp2_map = []
         # List the downloaded images
-        downloaded_files = sorted(glob.glob(os.path.join(save_path, '*.jp2')))
+        downloaded_files = sorted(glob.glob(os.path.join(self.save_dir, '*.jp2')))
         # Parse filenames to get the actual image time
         jp2_datetimes = [datetime_from_filename(filename) for filename in downloaded_files]
         n_incomplete_groups = 0
-        # Create the "rejection" subdirectory to move the image.
-        reject_dir = os.path.join(save_path, self.reject_subdir)
-        os.makedirs(reject_dir, exist_ok=True)
         # Loop over all the hek times and test if we have all measurements after the download process
         # Reject the whole group if that's not the case
-        for i, time_in in enumerate(times):
+        for i, time_in in enumerate(self.times):
             hek_time = parse_time(time_in)
             # Get the index of files whose time fall within a 2hr window centered on the hek event time
             tmatches = [i for i, file_time in enumerate(jp2_datetimes) if hek_time - self.dt < file_time < hek_time + self.dt]
@@ -184,7 +174,22 @@ class Jp2ImageDownload:
                 n_incomplete_groups += 1
                 # Move files in that incomplete group for rejection in seperate directory
                 for f in tmatches:
-                    shutil.move(downloaded_files[f], reject_dir)
+                    # The same file may have already been move if the hek time matched again to that file
+                    # so check first it exist
+                    if os.path.isfile(downloaded_files[f]):
+                        shutil.move(downloaded_files[f], os.path.join(self.reject_dir, os.path.basename(downloaded_files[f])))
+            else:
+                # Append all files of the group to the hek_time <-> jp2 map
+                jp2_files = [downloaded_files[t] for t in tmatches]
+                hek_time_jp2_map_entry = [hek_time] + jp2_files
+                hek_time_jp2_map.append(hek_time_jp2_map_entry)
+
+        # Write hek_time_jp2_map to a csv file
+        with open(self.hek_time_jp2_map_csv, 'w') as csvFile:
+            writer = csv.writer(csvFile)
+            writer.writerows(hek_time_jp2_map)
+        csvFile.close()
+
 
 
     def make_labels(self):
@@ -193,45 +198,46 @@ class Jp2ImageDownload:
         in the subdirectory 'label_masks' under the image directory.
         """
 
-        for ii, download_date in enumerate(self.date_list):
-            print('Making label-masks at date: {:s}'.format(download_date.strftime(self.data_subdir_format)))
-            subdir = download_date.strftime(self.data_subdir_format)
-            image_dir = os.path.join(self.parent_dir, subdir)
-            label_save_path = os.path.join(self.parent_dir, subdir, 'label_masks')
-            os.makedirs(label_save_path, exist_ok=True)
-            # First download the images. Feature extraction will be done separately
-            tstart = download_date
-            tend = tstart + timedelta(days=1)
+        # Get a list of existing files (if any)
+        jp2f = sorted(glob.glob(os.path.join(self.save_dir, '*.jp2')))
+        # Parse filenames to get the actual image time
+        jp2_datetimes = [datetime_from_filename(filename) for filename in jp2f]
 
-            # Get a list of existing files (if any)
-            jp2f = sorted(glob.glob(os.path.join(image_dir, '*.jp2')))
-            # Parse filenames to get the actual image time
-            jp2_datetimes = [datetime_from_filename(filename) for filename in jp2f]
+        result, times = get_hek_result(self.tstart, self.tend)
 
-            result, times = get_hek_result(tstart, tend)
+        ch = [elem for elem in result if elem['event_type'] == 'CH']
+        ar = [elem for elem in result if elem['event_type'] == 'AR']
+        ss = [elem for elem in result if elem['event_type'] == 'SS']
 
-            ch = [elem for elem in result if elem['event_type'] == 'CH']
-            ar = [elem for elem in result if elem['event_type'] == 'AR']
-            ss = [elem for elem in result if elem['event_type'] == 'SS']
+        mask_time_map = []
 
-            for i, time_in in enumerate(times):
-                hek_time = parse_time(time_in)
-                # Get closest image
-                nearest_datetime = nearest(jp2_datetimes, hek_time)
-                nearest_file = jp2f[jp2_datetimes.index(nearest_datetime)]
-                print('...processing hek time: {:s} at index {:d} '.format(hek_time.strftime('%Y/%m/%d %H:%M:%S'), i))
-                print('......using nearest image at time: {:s}'.format(nearest_datetime.strftime('%Y/%m/%d %H:%M:%S')))
+        for i, time_in in enumerate(times):
+            hek_time = parse_time(time_in)
+            # Get closest image
+            nearest_datetime = nearest(jp2_datetimes, hek_time)
+            nearest_file = jp2f[jp2_datetimes.index(nearest_datetime)]
+            print('...processing hek time: {:s} at index {:d} '.format(hek_time.strftime('%Y/%m/%d %H:%M:%S'), i))
+            print('......using nearest image at time: {:s}'.format(nearest_datetime.strftime('%Y/%m/%d %H:%M:%S')))
 
-                ch_list = [elem for elem in ch if elem['event_starttime'] == time_in]
-                ar_list = [elem for elem in ar if elem['event_starttime'] == time_in]
-                ss_list = [elem for elem in ss if elem['event_starttime'] == time_in]
+            ch_list = [elem for elem in ch if elem['event_starttime'] == time_in]
+            ar_list = [elem for elem in ar if elem['event_starttime'] == time_in]
+            ss_list = [elem for elem in ss if elem['event_starttime'] == time_in]
+            # The above 3 lists have typically only 1 that's not empty. Let's explicitly tell to not process any empty label list.
+            if ch_list:
+                ch_mask, ch_file_path = gen_label_mask(ch_list, nearest_file, hek_time, 'CH', save_path=self.label_save_dir, do_plot=True)
+                mask_time_map.append([ch_file_path, time_in])
+            if ar_list:
+                ar_mask, ar_file_path = gen_label_mask(ar_list, nearest_file, hek_time, 'AR', save_path=self.label_save_dir, do_plot=True)
+                mask_time_map.append([ar_file_path, time_in])
+            if ss_list:
+                ss_mask, ss_file_path = gen_label_mask(ss_list, nearest_file, hek_time, 'SS', save_path=self.label_save_dir, do_plot=True)
+                mask_time_map.append([ss_file_path, time_in])
 
-                print('.........processing CH')
-                ch_mask, _ = gen_label_mask(ch_list, nearest_file, hek_time, 'CH', save_path=label_save_path, do_plot=True)
-                print('.........processing AR')
-                ar_mask, _ = gen_label_mask(ar_list, nearest_file, hek_time, 'AR', save_path=label_save_path, do_plot=True)
-                print('.........processing SS')
-                ss_mask, _ = gen_label_mask(ss_list, nearest_file, hek_time, 'SS', save_path=label_save_path, do_plot=True)
+        # Write mask_time_map to a csv file
+        with open(self.mask_hek_time_map_csv, 'w') as csvFile:
+            writer = csv.writer(csvFile)
+            writer.writerows(mask_time_map)
+        csvFile.close()
 
 
 def gen_label_mask(label_list, image_filepath, hek_time, label, save_path=None, do_plot=False):
@@ -239,20 +245,21 @@ def gen_label_mask(label_list, image_filepath, hek_time, label, save_path=None, 
     Create a binary mask of feature locations. If there is no feature, an empty mask is returned
 
     :param label_list: List of HEK dictionaries
-    :param image_filepath: file path of image in which the feature is detected
+    :param image_filepath: file path of image closest to label. Its header is used for converting hek event coordinates.
     :param hek_time: Datetime used to write the filenames of the mask files
+    :param label: The event type being processed: either 'CH', 'AR' or 'SS'.
     :param save_path: directory of the mask files and optionally the figures.
     :param do_plot: True will plot figures and save them to save_path
     :return: binary mask of feature and list of polygon vertices tuples.
     """
 
-
+    print('.........processing ' + label)
     jp2_header = get_header(image_filepath)[0]
     aia_wcs = WCS(jp2_header)
     mask_shape = [4096, 4096]
     mask = np.zeros(mask_shape, dtype=np.int)
-    verts_yx_list = []
     dummy_array = np.empty(mask_shape, dtype=np.int)
+    verts_yx_list = []
 
     if aia_wcs.array_shape != mask.shape:
         raise ValueError('Mask and WCS array shapes do not agree.')
@@ -260,7 +267,7 @@ def gen_label_mask(label_list, image_filepath, hek_time, label, save_path=None, 
     aia_map = Map(dummy_array, aia_wcs)
 
     if do_plot:
-        fig = plt.figure()
+        fig = plt.figure(figsize=(10, 10))
         ax = plt.subplot(projection=aia_map)
         aia_map.plot(axes=ax)
     # parsing boundary coordinate string for each feature
@@ -272,32 +279,33 @@ def gen_label_mask(label_list, image_filepath, hek_time, label, save_path=None, 
         print(labels["hpc_boundcc"])
         print(p3)
 
-
         boundary_coords = SkyCoord([(float(v[0]), float(v[1])) * u.arcsec for v in p3], frame=aia_map.coordinate_frame)
 
         # Vertices  of feature
-        # R: This can contain NaN. The output of SkyCoord needs to be sanitized
         pixel_verts = aia_map.world_to_pixel(boundary_coords)
         print(pixel_verts)
         verts_x = np.array([x for x in pixel_verts[0].value if not np.isnan(x)])
         verts_y = np.array([y for y in pixel_verts[1].value if not np.isnan(y)])
         verts_yx = np.round(np.array((verts_y, verts_x)).T).astype(np.int)
         verts_yx_list.append(verts_yx)
+        # The mask is populated in-place -> accross different instances of the same hek event, the mask builds itself.
         fill_polygon(verts_yx, mask)
-
-        write_mask(mask, hek_time, label, save_path=save_path)
 
         if do_plot:
             ax.plot_coord(boundary_coords, color='r')
+
+    mask_file_path = write_mask(mask, hek_time, label, save_path=save_path)
+
 
     if do_plot:
         label_map = Map(mask, aia_wcs)
         label_map.plot(axes=ax)
         plt.tight_layout()
         plt.savefig(os.path.join(save_path, os.path.basename(image_filepath)[0:-4] + '_plot_' + label + '.png'))
+        plt.close(fig)
 
 
-    return mask, verts_yx_list
+    return mask, mask_file_path
 
 
 
@@ -416,13 +424,15 @@ def write_mask(mask_in, label_date, label='Mask', save_path=''):
     :param label_date: time of the label detection as Datetime
     :param label: String of feature type e.g. "SS", "CH", or "AR"
     :param save_path: save path for saving mask as image
-    :return: none
+    :return: path to the saved file
     """
 
     label_mask_name = label_date.strftime("%Y_%m_%d__%H_%M_%S")+'__'+label+'_mask'
     save_mask_name = os.path.join(save_path, label_mask_name)
 
     np.savez_compressed(save_mask_name, mask_in.astype(np.bool))
+
+    return save_mask_name
 
 
 
