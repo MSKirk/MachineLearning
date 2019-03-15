5a7f51253eab5e84a38e5346c3ba9391278e59ce
Michael Kirk <michael.s.kirk@nasa.gov>
2019-03-15 16:37:07 -0400

    Update script_over_many_months to catch server errors

--- script_over_many_months.py
+++ script_over_many_months.py
@@ -1,7 +1,8 @@
 import os
 import JP2_Image_Download as Jpd
 from dateutil.rrule import rrule, MONTHLY 
-from sunpy.time import parse_time  
+from sunpy.time import parse_time
+import logging  
 
 
 #save_dir = os.path.join(os.path.expanduser('~'), 'Data/michael/MachineLearning/Hek_project/')
@@ -12,12 +13,18 @@ end_list = [dt for dt in rrule(MONTHLY, dtstart=parse_time('2010/07/31 23:30:00'
 
 for tstart, tend in zip(begin_list, end_list):
 
-	j = Jpd.Jp2ImageDownload(save_dir, tstart=tstart.strftime('%Y/%m/%d %H:%M:%S'), tend=tend.strftime('%Y/%m/%d %H:%M:%S'))
+	j = Jpd.Jp2ImageDownload(save_dir, tstart=tstart, tend=tend)
+	logging.basicConfig(format='%(asctime)s %(message)s', filename=os.path.join(j.save_dir, 'logger.log'), level=logging.DEBUG)
 
 	while j.download_flag:
-		j.download_images()
-		if j.missed_downloads:
-			print('Missed downloads. Making new attempt(s)...')
+		try:
+			j.download_images()
+			if j.missed_downloads:
+				print('Missed downloads. Making new attempt(s)...')
+		except ConnectionResetError:
+			print('HEK server error. Trying again...')
+			logging.debug('HEK server error raised ConnectionResetError')
+			continue
 
 	j.data_cleanup()
 
