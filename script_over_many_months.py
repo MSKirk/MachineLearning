b90dfb6af1dea2bf90ff860b051a6b0245439dca
Michael Kirk <michael.s.kirk@nasa.gov>
2019-03-18 13:37:29 -0400

    Incorporated RA changes.

--- script_over_many_months.py
+++ script_over_many_months.py
@@ -6,35 +6,43 @@ from sunpy.time import parse_time
 import logging  
 
 
-# SET THESE PARAMETERS
+if __name__ == '__main__':
 
-#save_dir = os.path.join(os.path.expanduser('~'), 'Data/michael/MachineLearning/Hek_project/')
-save_dir = os.path.abspath('/Volumes/SolarData/LabledImages')
+    # SET THESE PARAMETERS:
+    save_dir = os.path.abspath('/Volumes/SolarData/LabledImages')
+    #save_dir = os.path.abspath('/Volumes/RAPH_1TB/Data/Michael/Hek_project')
+    start_date = '2010/06/01 00:00:00'  # inclusive
+    end_date = '2013/06/01 00:00:00'  # not inclusive
 
-start_date = '2010/06/01 00:00:00'  # inclusive
-end_date = '2013/06/01 00:00:00'  # not inclusive
+    # SHOULDN'T NEED TO CHANGE ANYTHING BELOW THIS --------->>
 
-# SHOULDN'T NEED TO CHANGE BELOW THIS
-begin_list = [dt for dt in rrule(MONTHLY, dtstart=parse_time(start_date), until=parse_time(end_date))]
-end_list = [ii - datetime.timedelta(minutes=30) for ii in begin_list]
+    begin_list = [dt for dt in rrule(MONTHLY, dtstart=parse_time(start_date), until=parse_time(end_date))]
+    end_list = [ii - datetime.timedelta(minutes=30) for ii in begin_list]
 
-for tstart, tend in zip(begin_list[:-1], end_list[1:]):
+    for tstart, tend in zip(begin_list[:-1], end_list[1:]):
 
-    j = Jpd.Jp2ImageDownload(save_dir, tstart=tstart, tend=tend)
-    logging.basicConfig(format='%(asctime)s %(message)s', filename=os.path.join(j.save_dir, 'logger.log'), level=logging.DEBUG)
+        j = Jpd.Jp2ImageDownload(save_dir, tstart=tstart, tend=tend)
 
-    while j.download_flag:
-        try:
-            j.download_images()
-            if j.missed_downloads:
-                print('Missed downloads. Making new attempt(s)...')
-        except ConnectionResetError:
-            print('HEK server error. Trying again...')
-            logging.debug('HEK server error raised ConnectionResetError')
-            continue
+        logging.basicConfig(format='%(asctime)s %(message)s', filename=os.path.join(j.save_dir, 'logger.log'), level=logging.INFO)
 
-    j.data_cleanup()
+        while j.missed_downloads_flag:
+            try:
+                j.download_images()
+                if j.missed_downloads:
+                    print('Missed downloads. Making new attempt(s)...')
+            except ConnectionResetError:
+                print('HEK server error. Trying again...')
+                logging.warning('HEK server error raised ConnectionResetError')
+                continue
 
-    # Test the creation of label-masks
-    j.make_labels()
+        j.data_cleanup()
 
+        # Test the creation of label-masks
+
+        while j.missed_labels_flag:
+            try:
+                j.make_labels()
+            except ConnectionResetError:
+                print('HEK server error during make_labels(). Trying again...')
+                logging.warning('HEK server error raised ConnectionResetError during make_labels()')
+                continue
