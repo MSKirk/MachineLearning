6a71965a12d6f13235fef230c11ed1f4667e34ec
MSKirk <michael.s.kirk@gmail.com>
2019-05-23 18:00:05 -0400

    Correcting for the rotation issue and ccd degredation issue.

---
+++ read_jp2.py
@@ -0,0 +1,32 @@
+import numpy as np
+from astropy import time
+from astropy import units as u
+import sunpy.io
+import calibration
+
+# pull out effective area class to avoid rereading the table
+
+aia_effective_area = calibration.AIAEffectiveArea()
+
+
+def read_solar_jp2(filepath, verbose=False):
+
+    img = sunpy.io.read_file(filepath, filetype='jp2')[0]
+
+    if img.header['CROTA2'] != 0:
+        if verbose:
+            print('Rotating image to solar north')
+        data = calibration.scale_rotate(img.data, img.header['CROTA2'])
+    else:
+        data = img.data
+
+    if verbose:
+        print('Correcting for CCD degradation')
+
+    data /= aia_effective_area.effective_area_ratio(img.header['WAVELNTH']*u.AA, time.Time(img.header['DATE-OBS']).to_datetime())
+
+    return data
+
+
+
+
