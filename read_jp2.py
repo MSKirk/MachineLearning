898f3604e931cb67365556644f9fa2d5c5fa0c92
MSKirk <michael.s.kirk@gmail.com>
2019-05-24 12:49:29 -0400

    Type error bug squash.

--- read_jp2.py
+++ read_jp2.py
@@ -3,8 +3,9 @@ from astropy import time
 from astropy import units as u
 import sunpy.io
 import calibration
+import warnings
 
-# pull out effective area class to avoid rereading the table
+# initialize effective area class to avoid rereading the calibration table
 
 aia_effective_area = calibration.AIAEffectiveArea()
 
@@ -23,9 +24,14 @@ def read_solar_jp2(filepath, verbose=False):
     if verbose:
         print('Correcting for CCD degradation')
 
-    prepped_data /= aia_effective_area.effective_area_ratio(img.header['WAVELNTH']*u.AA, time.Time(img.header['DATE-OBS']).to_datetime())
+    prepped_data = prepped_data * (1./aia_effective_area.effective_area_ratio(img.header['WAVELNTH']*u.AA, time.Time(img.header['DATE-OBS']).to_datetime()))
 
-
+    if img.header['WAVELNTH'] in [94, 131, 171, 193, 211, 304, 335]:
+        if np.sum(img.data == 0) / np.sum(img.data != 0) > 0.5:
+            warnings.warn('Significant amount of data missing in the image', UserWarning)
+    else:
+        if np.sum(img.data == 0) / np.sum(img.data != 0) > 1.2:
+            warnings.warn('Significant amount of data missing in the image', UserWarning)
 
     return prepped_data
 
