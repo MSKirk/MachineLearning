f335fb2c009c3b80a01255ee7a24ac65b5dc138c
Raphael Attie <attie.raphael@gmail.com>
2019-08-13 14:44:55 -0400

    - changed jp2 path to repository-relative path

--- sanity_checks/check_read_solar_jp2.py
+++ sanity_checks/check_read_solar_jp2.py
@@ -3,8 +3,7 @@ from read_jp2 import read_solar_jp2
 import matplotlib
 matplotlib.use('Tkagg')
 
-# Get a jp2 sample. Update to personal case
-filepath =\
-    os.path.expanduser('~/Data/ML_projects/aia_recognition/2011_06/jp2/2011_06_25__00_59_43_71__SDO_AIA_AIA_1700.jp2')
+# Get a jp2 sample. here using the jp2 included in this github repo
+filepath = '../images/2011_06_25__00_59_43_71__SDO_AIA_AIA_1700.jp2'
 
 pdata = read_solar_jp2(filepath)
